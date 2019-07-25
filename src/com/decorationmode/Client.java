08e5835bbdff2176f5d6952db945631629a84be2
lijun <lijun@git.com>
2019-07-25 21:27:36 +0800

    'decorationmode'

--- src/com/decorationmode/Client.java
+++ src/com/decorationmode/Client.java
@@ -1,16 +1,15 @@
 package com.decorationmode;
 
 import com.decorationmode.component.Drink;
-import com.decorationmode.component.LoveCoffee;
+import com.decorationmode.component.UsaCoffee;
 import com.decorationmode.decorator.Mango;
 import com.decorationmode.decorator.Tea;
 
 public class Client {
     public static void main(String[] args) {
-        Drink coffee = new LoveCoffee();
-        Drink order = new Mango(coffee);
-        Drink tea = new Tea(order);
-        Drink tea2 = new Tea(tea);
-        System.out.println(tea2.getDescribe());
+        Drink coffee = new UsaCoffee();
+        Drink tea = new Tea(coffee);
+        Drink mango = new Mango(tea);
+        System.out.println(mango.getDescribe());
     }
 }
